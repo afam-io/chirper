818410a0d354b779f1b2e2d4d5d9ff53b9a91b61
Afam Okoh <afamsdev@gmail.com>
2023-09-07 21:36:57 +0100

    feat: add front end view for tweet

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response
     {
-        //
-    }
+        return Inertia::render('Chirps/Index', [
+            //
+        ]);
+        }
 
     /**
      * Show the form for creating a new resource.
